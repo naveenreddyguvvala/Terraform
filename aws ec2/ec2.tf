8c83c99f46673827db4375a8bc09542cf403af78
Naveen <guvvala4835@gmail.com>
2025-10-31 19:16:20 +0530

    ec2

--- aws ec2/ec2.tf
+++ aws ec2/ec2.tf
@@ -1,26 +1,28 @@
-resource "aws_instance" "terraform"{
-    ami  = var.ami_id
-    instance_type = var.instance_type
+resource "aws_instance" "terraform" {
+    ami = "ami-0199d4b5b8b4fde0e"
+    instance_type = "t3.micro"
     vpc_security_group_ids = [aws_security_group.allow_all.id]
-    region = var.region
-    tags = var.ec2_tags 
+    tags = {
+        Name = "terraform-1"
+        Terraform = "true"
+    }
 }
 
 resource "aws_security_group" "allow_all" {
-  name   = var.sg_name
+  name   = "allow-all"
 
   egress {
-    from_port        = var.egress_from_port # from port 0 to to port 0 means all ports
-    to_port          = var.egress_to_port
-    protocol         = var.protocol2 # -1 means all protocols
-    cidr_blocks      = var.cidr1 # internet
+    from_port        = 0 # from port 0 to to port 0 means all ports
+    to_port          = 0 
+    protocol         = "-1" # -1 means all protocols
+    cidr_blocks      = ["0.0.0.0/0"] # internet
   }
 
   ingress {
-    from_port        = var.ingress_from_port # from port 0 to to port 0 means all ports
-    to_port          = var.ingress_to_port 
-    protocol         = var.protocol1 # -1 means all protocols
-    cidr_blocks      = var.cidr2 # internet
+    from_port        = 0 # from port 0 to to port 0 means all ports
+    to_port          = 0 
+    protocol         = "-1" # -1 means all protocols
+    cidr_blocks      = ["0.0.0.0/0"] # internet
   }
 
   tags = {
